8037d238e892dbe598440e1b8fa7e82814e63ceb
Zhengke <zhengke0509@foxmail.com>
2021-11-04 10:02:11 +0800

    ci(change 2021-11-04 10:01): 修改 配置

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router"; // 引入vue-router中的类型
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    path: "/login",
+    component: () => import("@/views/login/Login.vue"),
+  },
+  {
+    path: "/main",
+    component: () => import("@/views/main/Main.vue"),
+  },
+];
+
+const router = createRouter({
+  routes,
+  history: createWebHashHistory(),
+});
+
+export default router;
