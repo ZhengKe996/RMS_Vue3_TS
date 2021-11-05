d952f7df732f452dcc7c771f284dd86a71734483
Zhengke <zhengke0509@foxmail.com>
2021-11-05 19:32:45 +0800

    feat: change

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import router from "./router";
-import store from "./store";
+import router from './router'
+import store from './store'
 
-import { globalRegister } from "./global"; // 引入抽取出的第三方库
-import ownRequest from "./service/index"; // 对应的实例
+import { globalRegister } from './global' // 引入抽取出的第三方库
+import ownRequest from './service/index' // 对应的实例
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(globalRegister);
-app.use(router);
-app.use(store);
+app.use(globalRegister)
+app.use(router)
+app.use(store)
 
 ownRequest.request({
-  url: "/home/multidata",
-  method: "GET",
+  url: '/home/multidata',
+  method: 'GET',
   interceptors: {
     requestInterceptor: (config) => {
-      console.log("单独请求的config");
-      return config;
+      console.log('单独请求的config')
+      return config
     },
     responseInterceptor: (res) => {
-      console.log("单独请求的res");
-      return res;
-    },
-  },
-});
+      console.log('单独请求的res')
+      return res
+    }
+  }
+})
 
-app.mount("#app");
+app.mount('#app')
